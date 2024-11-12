274123f4a8f3e62c5ae5da27e18749353fe1e84a
F-Shouter <132644949+F-Shouter@users.noreply.github.com>
2024-11-12 14:03:20 -0300

    corrigido Artista.java com 5 atributos

--- project-music/back-end-crud-produto/src/main/java/umc/devapp/crud_produto/entity/Artista.java
+++ project-music/back-end-crud-produto/src/main/java/umc/devapp/crud_produto/entity/Artista.java
@@ -12,5 +12,9 @@ public class Artista {
     private String nome;
     @Column(name = "idade")
     private double idade;
+    @Column(name = "banda")
+    private string banda;
+    @Column(name = "pais")
+    private string pais;
     
 }
