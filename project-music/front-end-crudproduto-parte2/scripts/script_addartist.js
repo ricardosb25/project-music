1e2f249260aaf440b0d3496d1604853d969ae238
F-Shouter <yukari.yamasaki1809@gmail.com>
2024-11-15 15:25:36 -0300

    addartist.html e script_addartist.js corrigidos e pom.xml de java version com 21

---
+++ project-music/front-end-crudproduto-parte2/scripts/script_addartist.js
@@ -0,0 +1,32 @@
+function clearTextFields() {
+    document.getElementById("nome").value = "";
+    document.getElementById("idade").value = "";
+    document.getElementById("banda").value = "";
+    document.getElementById("pais").value = "";
+}
+
+async function addArtist() {
+    const formE1 = document.querySelector("#formadd");
+    const formData = new FormData(formE1);
+    const artist = Object.fromEntries(formData);
+    const url = "http://localhost:8080/artista/add";
+    const option = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(artist)
+    };
+
+    try {
+        const result = await fetch(url, option);
+        if (result.status === 201) {
+            clearTextFields();
+            alert('Cadastrado com sucesso');
+            location.reload(); 
+        } else {
+            alert('Erro ao cadastrar');
+        }
+    } catch (error) {
+        console.error('Erro:', error);
+        alert('Erro ao cadastrar');
+    }
+}
