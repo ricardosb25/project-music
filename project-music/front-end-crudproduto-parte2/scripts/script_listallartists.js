11318a427ab4d3e26d541ae6b46ce56968cad38b
F-Shouter <yukari.yamasaki1809@gmail.com>
2024-11-16 14:52:20 -0300

    function delete no script_listallartists apagado

--- project-music/front-end-crudproduto-parte2/scripts/script_listallartists.js
+++ project-music/front-end-crudproduto-parte2/scripts/script_listallartists.js
@@ -45,27 +45,4 @@ async function listAllArtists() {
     }
 }
 
-async function deleteArtist(id) {
-    const url = `http://localhost:8080/artista/delete/${id}`;
-    const option = {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    };
-
-    try {
-        const result = await fetch(url, option);
-        if (result.status === 204) {
-            alert('Deletado com sucesso');
-            location.reload(); 
-        } else {
-            alert('Erro ao deletar');
-        }
-    } catch (error) {
-        console.error('Erro:', error);
-        alert('Erro ao deletar');
-    }
-}
-
 listAllArtists();
