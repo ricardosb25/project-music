204ef83b16fbca0b0ca69fbfce6028cbe4962942
F-Shouter <yukari.yamasaki1809@gmail.com>
2024-11-16 14:16:14 -0300

    script_addartist.js atualizado

--- project-music/front-end-crudproduto-parte2/scripts/script_addartist.js
+++ project-music/front-end-crudproduto-parte2/scripts/script_addartist.js
@@ -1,32 +1,26 @@
-function clearTextFields() {
+function clearTextFields(){
     document.getElementById("nome").value = "";
     document.getElementById("idade").value = "";
     document.getElementById("banda").value = "";
     document.getElementById("pais").value = "";
 }
 
-async function addArtist() {
+async function addArtist(){
     const formE1 = document.querySelector("#formadd");
     const formData = new FormData(formE1);
     const artist = Object.fromEntries(formData);
     const url = "http://localhost:8080/artista/add";
     const option = {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        headers:{'Content-Type': 'application/json'},
         body: JSON.stringify(artist)
-    };
-
-    try {
-        const result = await fetch(url, option);
-        if (result.status === 201) {
-            clearTextFields();
-            alert('Cadastrado com sucesso');
-            location.reload(); 
-        } else {
-            alert('Erro ao cadastrar');
-        }
-    } catch (error) {
-        console.error('Erro:', error);
+    }
+    const result = await fetch(url, option);
+    if(result.status === 201){
+        clearTextFields();
+        alert('Cadastrado com sucesso');
+    }
+    else{
         alert('Erro ao cadastrar');
     }
 }
