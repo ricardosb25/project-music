84019004464e3f44518b65e96d7cefea15005cd1
F-Shouter <yukari.yamasaki1809@gmail.com>
2024-11-23 14:43:36 -0300

    adicionando listalldsongs

--- project-music/front-end-crudproduto-parte2/scripts/script_listallsongs.js
+++ project-music/front-end-crudproduto-parte2/scripts/script_listallsongs.js
@@ -1,26 +1,9 @@
-async function listById() {
-    const id = document.getElementById("listbyid").value;
-    if (!id) {
-        alert("Por favor, insira um ID válido");
-        return;
-    }
-
-    const url = `http://localhost:8080/musica/list/${id}`;
-    try {
-        const result = await fetch(url, { method: "GET" });
-        if (result.status === 200) {
-            const song = await result.json();
-            showProduct(song);
-        } else {
-            alert('Música não encontradnameo');
-        }
-    } catch (error) {
-        console.error('Erro:', error);
-        alert('Erro ao buscar muúica');
-    }
+function clearLoading() {
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("listbyid").value = "";
 }
 
-function showProduct(song) {
+function showSongs(songs) {
     let tab = `
         <thead>
             <tr>
@@ -34,18 +17,32 @@ function showProduct(song) {
             </tr>
         </thead>
     `;
-
-    tab += `
-        <tr>
-            <td>${song.id}</td>
-            <td>${song.nome}</td>
-            <td>${song.ano}</td>
-            <td>${song.banda}</td>
-            <td>${song.album}</td>
-            <td><a href='updatesong.html?id=${song.id}'><img src="images/edit01.png" width="20" height="15"></a></td>
+    for (let song of songs) {
+        tab += `
+            <tr>
+                <td>${song.id}</td>
+                <td>${song.nome}</td>
+                <td>${song.ano}</td>
+                <td>${song.banda}</td>
+                <td>${song.album}</td>
+                <td><a href='updatesong.html?id=${song.id}'><img src="images/edit01.png" width="20" height="15"></a></td>
                 <td><button onclick="deleteSong(${song.id})"><img src="images/trash01.png" width="20" height="15"></button></td>
-        </tr>
-    `;
-
+            </tr>
+        `;
+    }
     document.getElementById("songs").innerHTML = tab;
 }
+
+async function listAllSongs() {
+    const url = "http://localhost:8080/songa/listall";
+    const dados = await fetch(url, { method: "GET" });
+    if (dados.status === 200) {
+        const songs = await dados.json();
+        if (songs) {
+            clearLoading();
+        }
+        showSongs(songs);
+    }
+}
+
+listAllSongs();
