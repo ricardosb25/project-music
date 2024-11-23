cd4fb0f21396dd77881d9ff27eeeb2ba9ea96c46
F-Shouter <yukari.yamasaki1809@gmail.com>
2024-11-23 12:54:54 -0300

    Musica.java imports certos agora

--- project-music/back-end-crud-produto/src/main/java/ry/project/music/entity/Musica.java
+++ project-music/back-end-crud-produto/src/main/java/ry/project/music/entity/Musica.java
@@ -1,9 +1,6 @@
 package ry.project.music.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
